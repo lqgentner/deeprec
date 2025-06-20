8a3a1152bfc4e532cdc4b5ff3e3b4ef3ba3fb89e
Luis Gentner <luis.gentner@outlook.com>
2025-06-20 16:08:39 +0200

    Make shapefile cleaning work with zips

--- scripts/2-clean-shapefiles.py
+++ scripts/2-clean-shapefiles.py
@@ -6,44 +6,31 @@ Usage:
     python scripts/2-clean-shapefiles.py
 """
 
-import logging
 from pathlib import Path
 
 import geopandas as gpd
 import janitor  # noqa: F401
+from loguru import logger
 import numpy as np
 
 from deeprec.utils import ROOT_DIR
 
 
 def main():
-    # Set up logger
-    log_fmt = "%(name)-12s: %(levelname)-8s %(message)s"
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-    logger = logging.getLogger(__name__)
-
     # Perform cleaning
-    mrb_in = ROOT_DIR / "data/raw/shapefiles/mrb"
-    mrb_out = ROOT_DIR / "data/processed/shapefiles/mrb/"
-    mrb_file = "mrb_basins.shp"
-    ne_in = ROOT_DIR / "data/raw/shapefiles/naturalearth"
-    ne_out = ROOT_DIR / "data/processed/shapefiles/naturalearth"
-    ne_file = "ne_50m_admin_0_countries.shp"
-    cnt_in = ROOT_DIR / "data/raw/shapefiles/continents"
-    cnt_out = ROOT_DIR / "data/processed/shapefiles/continents"
-    cnt_file = "World_Continents.shp"
-    mrb_out.mkdir(parents=True, exist_ok=True)
-    ne_out.mkdir(parents=True, exist_ok=True)
-    cnt_out.mkdir(parents=True, exist_ok=True)
+    mrb_in = ROOT_DIR / "data/raw/shapefiles/mrb/GRDC_Major_River_Basins_shp.zip"
+    mrb_out = ROOT_DIR / "data/processed/shapefiles/mrb/mrb_basins.shp"
+
+    ne_in = ROOT_DIR / "data/raw/shapefiles/naturalearth/ne_50m_admin_0_countries.zip"
+    ne_out = (
+        ROOT_DIR / "data/processed/shapefiles/naturalearth/ne_50m_admin_0_countries.shp"
+    )
 
     logger.info("Cleaning Major River Basins...")
-    clean_mrb(mrb_in / mrb_file, mrb_out / mrb_file)
+    clean_mrb(mrb_in, mrb_out)
 
     logger.info("Cleaning Natural Earth countries...")
-    clean_naturalearth(ne_in / ne_file, ne_out / ne_file)
-
-    logger.info("Cleaning continents...")
-    clean_continents(cnt_in / cnt_file, cnt_out / cnt_file)
+    clean_naturalearth(ne_in, ne_out)
 
     logger.info("Cleaning completed.")
 
@@ -64,6 +51,8 @@ def clean_mrb(file_in: Path, file_out: Path):
         ~gdf.riverbasin.duplicated(),
         gdf.riverbasin + "_" + gdf.groupby("riverbasin").cumcount().astype(str),
     )
+    # Make sure folder exists
+    file_out.parent.mkdir(parents=True, exist_ok=True)
     # Export cleaned shapefile
     gdf.to_file(file_out)
 
@@ -77,18 +66,8 @@ def clean_naturalearth(file_in: Path, file_out: Path):
     gdf = gdf.clean_names()
     # Remove spaces in admin
     gdf.admin = gdf.admin.str.replace(" ", "")
-    gdf.to_file(file_out)
-
-
-def clean_continents(file_in: Path, file_out: Path):
-    """Clean World continents shapefile"""
-    gdf = gpd.read_file(file_in, engine="pyogrio")
-    # Clean all caps column headers, drop cols
-    gdf = gdf.clean_names().drop(["objectid_1", "sqmi"], axis=1)
-    # Remove spaces in names
-    gdf.continent = gdf.continent.str.replace(" ", "")
-    # To WGS 84
-    gdf = gdf.to_crs(epsg=4326)
+    # Make sure folder exists
+    file_out.parent.mkdir(parents=True, exist_ok=True)
     gdf.to_file(file_out)
 
 
